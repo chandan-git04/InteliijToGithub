4ae9cd7a56f4458f92781014406e27d660ed9179
Chandan <pchandan0411@gmail.com>
2024-08-19 09:42:10 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+abstract class Shape {
+    abstract void draw();
+}
+
+class Circle extends Shape {
+    void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+
+class Rectangle extends Shape {
+    void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Shape shape = new Circle();
+        shape.draw();
+
+}
+}
